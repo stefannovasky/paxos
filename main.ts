cce6bb18fc5c6e6c891886a97438accd4501dfae
stefannovasky <stefannovasky@gmail.com>
2024-04-24 23:35:05 -0300

    refac

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 import fetch from "node-fetch";
 import express from "express";
 
+type Result<Error, Success> =
+  | { status: "error"; message: Error }
+  | { status: "ok"; data: Success };
+
 type ProposeValue = {
   id: number;
   value: string | null;
@@ -18,16 +22,22 @@ const nodes: Node[] = [
   { url: 'http://paxos_3003:3003' }
 ];
 
-const sendPostRequest = async <T>(url: string, bodyJson: string): Promise<T> => {
+const sendPostRequest = async <T>(url: string, bodyJson: string): Promise<Result<string, T>> => {
   const response = await fetch(url,
     { body: bodyJson, method: "POST", headers: { "Content-Type": "application/json" } });
-  return response.json() as T;
+
+  const responseBody = await response.json();
+  if (response.status !== 200) {
+    return { status: "error", message: (responseBody as { message: string }).message };
+  }
+
+  return { status: "ok", data: responseBody as T };
 }
 
 class Proposer {
   private id: number = 0;
 
-  public async prepare(proposeValue: string) {
+  public async prepare(proposeValue: string): Promise<Result<string, null>> {
     console.log(`[Proposer] starting preparation with value: ${proposeValue}`);
     this.id++;
 
@@ -37,12 +47,15 @@ class Proposer {
       const url = `${node.url}/acceptor/handle-prepare-message`;
       console.log(`[Proposer] sending request to: ${url} with body: ${prepareMessageBody}`);
       const response = await sendPostRequest<ProposeValue>(url, prepareMessageBody);
-      prepareMessagesResponses.push(response);
+      if (response.status === "error") {
+        return response;
+      }
+      prepareMessagesResponses.push(response.data);
     }
 
     const majority = (nodes.length / 2) + 1;
     if (prepareMessagesResponses.length + 1 < majority) {
-      throw new Error("didn't received a promise from the majority of acceptors. ");
+      return { status: "error", message: "didn't received a promise from the majority of acceptors. " }
     }
 
     let acceptedResponse = { id: 0, value: null } as ProposeValue;
@@ -63,18 +76,23 @@ class Proposer {
       var url = `${node.url}/acceptor/handle-propose`;
       console.log(`[Proposer] sending request to ${url} with body: ${proposeMessageBody}`);;
       const response = await sendPostRequest<ProposeValue>(url, proposeMessageBody);
-      proposesResults.push(response);
+      if (response.status === "error") {
+        return response;
+      }
+      proposesResults.push(response.data);
     }
 
     if ((proposesResults.length + 1) < majority) {
-      throw new Error("value not accepted by the majority. ");
+      return { status: "error", message: "value not accepted by the majority. " };
     }
 
     if (acceptedResponse.value !== null) {
-      throw new Error("another value has already been accepted. ");
+      return { status: "error", message: "another value has already been accepted. " };
     }
 
-    console.log(`value ${proposeValue} accepted successfully`);
+    console.log(`[Proposer] value ${proposeValue} accepted successfully`);
+
+    return { status: "ok", data: null };
   }
 }
 
@@ -82,11 +100,11 @@ class Acceptor {
   private maxId: number = 0;
   private acceptedPropose: Propose | null = null;
 
-  public prepare(prepareId: number): ProposeValue {
+  public prepare(prepareId: number): Result<string, ProposeValue> {
     console.log(`[Acceptor] starting preparation with id: ${prepareId}`)
 
     if (prepareId < this.maxId) {
-      throw new Error("a proposal with a higher ID has already been accepted. ");
+      return { status: "error", message: "a proposal with a higher ID has already been accepted." };
     }
 
     this.maxId = prepareId;
@@ -94,24 +112,24 @@ class Acceptor {
     if (this.acceptedPropose !== null) {
       const value = this.acceptedPropose.data.value;
       console.log(`[Acceptor] finishing preparation with id: ${prepareId} and value: ${value}`);
-      return { id: prepareId, value: value };
+      return { status: "ok", data: { id: prepareId, value: value } };
     }
 
     console.log(`[Acceptor] finishing preparation with id: ${prepareId} and value: null`);
-    return { id: prepareId, value: null };
+    return { status: "ok", data: { id: prepareId, value: null } };
   }
 
-  public propose(propose: Propose): ProposeValue {
+  public propose(propose: Propose): Result<string, ProposeValue> {
     console.log(`[Acceptor] starting proposal with id: ${propose.data.id} and value: ${propose.data.value}`);
 
     if (this.maxId != propose.data.id) {
-      throw new Error("cannot accept proposal with a lower id. ");
+      return { status: "error", message: "cannot accept proposal with a lower id. " };
     }
 
     this.acceptedPropose = propose;
     console.log(`[Acceptor] finishing proposal with id: ${propose.data.id} and value: ${propose.data.value}`);
 
-    return propose.data;
+    return { status: "ok", data: propose.data };
   }
 }
 
@@ -123,7 +141,11 @@ app.use(express.json());
 
 app.post("/proposer", async (req, res) => {
   console.log(`[${req.url}] with body: ${JSON.stringify(req.body)}`);
-  await proposer.prepare(req.body.value);
+  const result = await proposer.prepare(req.body.value);
+  if (result.status === "error") {
+    console.error(`[${req.url}] with body: ${JSON.stringify(req.body)} not accepted. message: ${result.message}`);
+    return res.status(400).send(result.message);
+  }
   console.log(`[${req.url}] with body: ${JSON.stringify(req.body)} accepted`);
   return res.status(200).send("Ok");
 });
@@ -131,15 +153,23 @@ app.post("/proposer", async (req, res) => {
 app.post("/acceptor/handle-prepare-message", (req, res) => {
   console.log(`[${req.url}] with body: ${JSON.stringify(req.body)}`);
   const result = acceptor.prepare(req.body.id);
+  if (result.status === "error") {
+    console.error(`[${req.url}] with body: ${JSON.stringify(req.body)} not accepted. message: ${result.message}`);
+    return res.status(400).send(result.message);
+  }
   console.log(`[${req.url}] with body returning: ${JSON.stringify(result)}`);
-  return res.status(200).json(result);
+  return res.status(200).json(result.data);
 });
 
 app.post("/acceptor/handle-propose", (req, res) => {
   console.log(`[${req.url}] with body: ${JSON.stringify(req.body)}`);
   const result = acceptor.propose(req.body);
+  if (result.status === "error") {
+    console.error(`[${req.url}] with body: ${JSON.stringify(req.body)} not accepted. message: ${result.message}`);
+    return res.status(400).send(result.message);
+  }
   console.log(`[${req.url}] with body ${JSON.stringify(req.body)} returning: ${JSON.stringify(result)}`);
-  return res.status(200).json(result);
+  return res.status(200).json(result.data);
 });
 
 const port = process.env.PORT;
